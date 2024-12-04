25a9b3d87cabdbf9055c800e47e5c0e2c5eac17d
MarcosGiannini <marcogfa@gmail.com>
2024-12-04 07:42:57 +0100

    Tema 1 del máster en curso

--- Apuntes y ejercicios del master/Pruebas/playground.js
+++ Apuntes y ejercicios del master/Pruebas/playground.js
@@ -1,27 +1,13 @@
-// Declare a variable for a and b
-let a = 1;
-let b = 4;
-const sum = a + b;
-const rest = a - b;
-const mult = a * b;
-const div = a / b;
-const power = a ** b;
-const mod = a % b;
+// Declare all a variables:
+let age = 30;
+const name = "Marcos";
+let isStudent = true;
+let address = null;
+const phone = undefined;
 
-// Print the first message
-console.log(`The sum of a and b is: ${sum}`);
-
-// Print the second message
-console.log(`The rest of a and b is: ${rest}`);
-
-// Print the third message
-console.log(`The multiplication of a and b is: ${mult}`);
-
-// Print the four message
-console.log(`The division of a and b is: ${div}`);
-
-// Print the five message
-console.log(`The power of a and b is: ${power}`);
-
-// Print the six message
-console.log(`The power of a and b is: ${mod}`);
+// Print the result:
+console.log(typeof age); 
+console.log(typeof name);
+console.log(typeof isStudent);
+console.log(typeof address);
+console.log(typeof phone);
