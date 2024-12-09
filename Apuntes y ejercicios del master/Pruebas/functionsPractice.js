57ddaac612dc93e8ec52961b31f2c6cd03056804
MarcosGiannini <marcogfa@gmail.com>
2024-12-09 06:55:45 +0100

    Updated exercises from Module 2, Session 2

--- Apuntes y ejercicios del master/Pruebas/functionsPractice.js
+++ Apuntes y ejercicios del master/Pruebas/functionsPractice.js
@@ -1,13 +1,7 @@
-// Calculate the average of all the numbers
-function average(...numbers) {
-    let total = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        // Sum all numbers in the array
-        total += numbers[i];
-    }
-    // Return the average by dividing the total sum by the number
-    return total / numbers.length;
+function squareNumber (a, b) {
+   const totalSquare = Math.pow(a, b);
+    return totalSquare;
 }
 
-let result = average(10, 50, 40);
-console.log(`The result to average is: ${result}`);
+const result = squareNumber(5, 5);
+console.log(`Total square number is: ${result}`);
